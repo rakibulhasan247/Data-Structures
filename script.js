57365adfb745ff3f61b1e6137109de07a841d62b
Rakibul Hasan Redoy <59376085+rakibulhasan247@users.noreply.github.com>
2022-01-31 23:49:38 +0600

    up

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-'use strict';
+// 'use strict';
 
 
 const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
 
 const restaurant = {
     resName: 'Classico Italiano',
@@ -11,21 +27,8 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours,
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
@@ -43,6 +46,28 @@ const restaurant = {
 };
 
 
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+    // console.log(day);
+    const open = restaurant.openingHours[day]?.open ?? 'Closed';
+    console.log(`On ${day}, we open at ${open}`);
+}
+
+//Method
+console.log(restaurant.order?.(1,2) ?? "Method doesn't exit");
+
+console.log(restaurant.orderRisotto?.(1,2) ?? "Method doesn't exit");
+
+//Arrays
+
+ for(const day of Object.keys(openingHours))
+ console.log(day);
+
+
+/* 
 
 const game = {
     team1: 'Bayern Munich',
@@ -84,6 +109,37 @@ const game = {
         team2: 6.5,
     },
 };
+
+
+
+
+/* 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+
+//Previous Way
+// let a = "";
+// for (let i = 0; i < menu.length; i++) {
+//     a = menu[i];
+//     console.log(`${i+1} = ${a}`);
+// }
+
+
+//The for of Loop
+for (const item of menu)
+    console.log(item);
+
+
+for (const item of menu.entries()) {
+    console.log(`${item[0]+1}: ${item[1]}`);
+};
+
+for (const [i, el] of menu.entries()) {
+    console.log(`${i+1}: ${el}`);
+};
+
+*/
+// console.log(...menu.entries());
 
 /* 
 // 1.
